b19a654fbd995229891a941a18db96ba5a3a5e76
SatishParmar1 <2021pietcssatish154@poornima.org>
2025-01-17 23:38:07 +0530

    sum pair exist

---
+++ Pairexist.java
@@ -0,0 +1,40 @@
+public class Pairexist {
+
+        // Function to check whether any pair exists
+        // whose sum is equal to the given target value
+        static boolean twoSum(int[] arr, int target){
+            int n = arr.length;
+    
+            // Iterate through each element in the array
+            for (int i = 0; i < n; i++) {
+              
+                // For each element arr[i], check every
+                // other element arr[j] that comes after it
+                for (int j = i + 1; j < n; j++) {
+                  
+                    // Check if the sum of the current pair
+                    // equals the target
+                    if (arr[i] + arr[j] == target) {
+                        return true;
+                    }
+                }
+            }
+          
+            // If no pair is found after checking
+            // all possibilities
+            return false;
+        }
+    
+        public static void main(String[] args){
+    
+            int[] arr = { 0, -1, 2, -3, 1 };
+            int target = -2;
+          
+            if (twoSum(arr, target))
+                System.out.println("true");
+            else
+                System.out.println("false");
+        }
+    }
+    
+
